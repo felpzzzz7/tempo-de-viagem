9509a898651a91429a6b56fe954e6706832df914
00001232657116s <33174974-00001232657116s@users.noreply.replit.com>
2024-05-22 11:55:36 +0000

    teste

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def estimar_tempo_viagem(distancia, velocidade_media):
 distancia = 200
 velocidade_media = 60
 tempo_estimado = estimar_tempo_viagem(distancia, velocidade_media)
-print("para uma viagem de {} km e velocidade média de {} km, o tempo estimado é de {}".format(distancia,velocidade_media,tempo_estimado))
+print("para umas viagem de {} km e velocidade média de {} km, o tempo estimado é de {}".format(distancia,velocidade_media,tempo_estimado))
